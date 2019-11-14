6d40dc3003806543c12c6f0c935bccfb1ae24e8d
Andrew Beeken <abeeken@gmail.com>
2019-11-14 15:06:30 +0000

    Lots of things!

--- app.js
+++ app.js
@@ -1,42 +1,114 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require('path');
 var fs = require('fs');
 var moment = require('moment');
 var crypto = require('crypto');
 var site = express();
+
 var port = 3000;
 
+/**
+ * Bodyparser puts post data into body
+ */
 site.use(bodyParser.urlencoded({extended: true}));
 
-site.use(express.static(__dirname + '/public'));
+/**
+ * Define render engine
+ */
+site.set('view engine', 'pug');
 
+/**
+ * Set views directory
+ */
+site.set('views', path.join(__dirname, '/src/views'));
+
+// CRUD
+
+/**
+ * /
+ */
 site.get('/', function(req, res){
-    res.sendFile(__dirname + '/public/index.html');
+    res.render('index');
 });
 
+/**
+ * /makelink
+ */
 site.get('/makelink', function(req, res){
-    res.sendFile(__dirname + '/public/makelink.html');
+    res.render('makelink');
 });
 
+/**
+ * /addrecord
+ */
 site.post('/addrecord', function(req, res){
     var date = moment().format('YYYY-MM-DD:hh:mm:ss');
     var data = req.body;
     data['date'] = date;
     var filename = data.name+data.email+data.date;
     filename = crypto.createHash('md5').update(filename).digest('hex');
-    fs.writeFile("links/"+filename+".txt", JSON.stringify(data), function(err) {
+    fs.writeFile(path.join(__dirname, 'links/'+filename+'.txt'), JSON.stringify(data), function(err) {
         if (err) console.log(err);
         console.log("File written!");
     });
-    res.sendFile(__dirname + '/public/addrecord.html');
+    url = req.protocol+'://'+req.headers.host+'/link?linkid='+filename;
+    res.render('addrecord', {url: url} );
 });
 
+/**
+ * /link
+ */
 site.get('/link', function(req, res){
-    fs.readFile("links/foo.txt", "utf-8", function(err, data) {
-        if(err) { console.log(err) }
-        data = JSON.parse(data);
+    var linkid = req.query.linkid;
+    console.log(linkid);
+    var filename = (path.join(__dirname, 'links/'+linkid+'.txt'));
+    fs.readFile(filename, "utf-8", function(err, data) {
+        if(err) {
+            console.log(err)
+        } else {
+            data = JSON.parse(data);
+            if(checkDate(data.date,"24")){
+                console.log(data);
+            } else {
+                fs.unlink(filename, function(err) {
+                    if(err){
+                        console.log(err)
+                    } else {
+                        console.log("Date invalid - file deleted.");
+                    }
+                });
+            }
+        }
     });
-    res.sendFile(__dirname + '/public/link.html');
+    res.render('link');
 });
 
+/**
+ * App
+ */
 site.listen(port, () => console.log(`App listening on port ${port}`));
+
+
+// Fns
+
+/**
+ * 
+ * @param {Date} date 
+ * @param {Number} timeout
+ * @returns Boolean
+ */
+function checkDate(date,timeout){
+    // Checks a date against a timeout in hours - returns true if the date is valid, false if not
+    var currentdate = new Date(moment().format('YYYY-MM-DD:kk:mm:ss'));
+    var date = new Date(date);
+    var datediff = Math.abs(currentdate - date) / 36e5;
+    console.log(date);
+    console.log(currentdate);
+    console.log(datediff);
+    if(datediff >= timeout){
+        return false;
+    } else {
+        return true;
+    }
+}
